f64c403e90494e63c8e2fa17c2a3553e68832559
xuetianw <xuetianw@sfu.ca>
2019-09-03 23:44:50 -0700

    add play drum sounds buttons and music playing UDP interface working properly

--- Beat-Box-Server/public/javascripts/beatbox_ui.js
+++ Beat-Box-Server/public/javascripts/beatbox_ui.js
@@ -4,8 +4,8 @@
 // Make connection to server when web page is fully loaded.
 const socket = io()
 $(document).ready(function() {
-	$('#modeNothing').click(function(){
-		sendPrimeCommand("none");
+	$('#modeNone').click(function(){
+		sendPrimeCommand("none\n");
 	});
 	$('#modeRock1').click(function(){
 		sendPrimeCommand("rock 1\n");
@@ -16,22 +16,32 @@ $(document).ready(function() {
 	$('#volumeDown').click(function(){
 		sendPrimeCommand("decrease volume\n");
 	});
-	$('#volumeUp').click(function(){
-		sendPrimeCommand("increase volume");
-	});
-	$('#PCMDown').click(function(){
-		sendPrimeCommand("decrease BPM\n");
-	});
-	$('#PCMUp').click(function(){
-		sendPrimeCommand("increase BPM\n");
-	});
+    $('#volumeUp').click(function(){
+        sendPrimeCommand("increase volume\n");
+    });
+    $('#BPMown').click(function(){
+        sendPrimeCommand("decrease BPM\n");
+    });
+    $('#BPMUp').click(function(){
+        sendPrimeCommand("increase BPM\n");
+    });
+    $('#Hi-Hat').click(function(){
+        sendPrimeCommand("hi-hat\n");
+    });
+    $('#Snare').click(function(){
+        sendPrimeCommand("snare\n");
+    });
+    $('#Base').click(function(){
+        sendPrimeCommand("base\n");
+    });
 
 	socket.on('commandReply', function(result) {
 		var newDiv = $('<div></div>').text(result);
 		$('#messages').append(newDiv);
 		$('#messages').scrollTop($('#messages').prop('scrollHeight'));
 	});
-	
+    window.setInterval(function() {sendRequest('uptime')}, 1000);
+
 });
 
 function sendPrimeCommand(message) {
